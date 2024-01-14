e5ea846c2cd65fb81ce433e1ee87dfa4f7c278ea
not-soo-cool <yashvendrasengar1111@gmail.com>
2024-01-14 23:12:48 +0530

    Cors changes 1

--- controller/User.js
+++ controller/User.js
@@ -63,7 +63,7 @@ export const login = async (req, res) => {
       .status(200)
       .cookie("token", token, {
         expires: new Date(Date.now() + 90*24*60*60*1000),
-        // httpOnly: true,
+        httpOnly: true,
       })
       .json({
         success: true,
