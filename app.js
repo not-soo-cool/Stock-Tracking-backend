a625ce7c3baba2443ae0ae2ad38646976c48e3ce
not-soo-cool <yashvendrasengar1111@gmail.com>
2024-01-18 01:41:02 +0530

    Okay

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    // origin: process.env.WEB_URL,
+    origin: "http://localhost:3000/",
     origin: process.env.WEB_URL,
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
