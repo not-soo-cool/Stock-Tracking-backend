eec31ae66a5480f88fe1caca4f4914c97d078213
not-soo-cool <yashvendrasengar1111@gmail.com>
2024-01-17 22:24:10 +0530

    Web Url updated

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.WEB_URL,
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
